20b0ce8f4befa37838fd1f80549e20dde57867d3
Indro <indro.malleus@khk.ee>
2015-09-22 10:04:06 +0300

    1429962-103877522 5. Kass koeraks

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -20,5 +20,26 @@
 <br>
 <br>
 <a href="#" onclick="alert('jääme siia!')">jääme siia!</a>
+<br>
+<br>
+
+<img id="myImage" onclick="changeImage()" src="cat.png" width="250" height="250">
+
+<p>Click on cat to see what happens!</p>
+
+<script>
+    function changeImage() {
+        var image = document.getElementById('myImage');
+        if (image.src.match("bulbon")) {
+            image.src = "cat.png";
+        } else {
+            image.src = "dog.png";
+        }
+    }
+</script>
+
+<br>
+
+
 </body>
 </html>
