ffeadf729c0ed2f8d207c04acc77a04a4f17da4b
Indro <indro.malleus@khk.ee>
2015-09-23 10:45:15 +0300

    1429962-103877532 6. jQuery kass koeraks done

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -3,9 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <title>Pseudoprojekt1</title>
+    <script src="jquery-2.1.4.min.js"></script>
 </head>
 <body>
 <h1>Pseudoprojekt</h1>
+
+<!--2. Nupp, mis kuvab hello world!-->
 
 <button onclick="myFunction()">Tere Maailm!</button>
 
@@ -16,21 +19,28 @@
 </script>
 <br>
 <br>
+<!--3. Nupp, mis kuvab suunab khk.ee!-->
+
 <a href="http://www.khk.ee" onClick="alert('Hello World!')">Tere Maailm!</a>
 <br>
 <br>
+
+<!--4. Nupp, mis kuvab j22me siia aga ei suuna kuskile!-->
+
 <a href="#" onclick="alert('jääme siia!')">jääme siia!</a>
 <br>
 <br>
 
-<img id="myImage" onclick="changeImage()" src="cat.png" width="250" height="250">
+<!--5. kass muutub koeraks!-->
+
+<img id="kasskoeraks" onclick="changeImage()" src="cat.png" width="300" height="250">
 
 <p>Click on cat to see what happens!</p>
 
 <script>
     function changeImage() {
-        var image = document.getElementById('myImage');
-        if (image.src.match("bulbon")) {
+        var image = document.getElementById('kasskoeraks');
+        if (image.src.match("dog")) {
             image.src = "cat.png";
         } else {
             image.src = "dog.png";
@@ -39,6 +49,27 @@
 </script>
 
 <br>
+
+
+<!--6. kass koeraks jquery!-->
+
+
+
+<div class="cat">
+    <img src="cat.png"
+</div>
+
+<script>
+    $("img[src='cat.png']").click(function () {
+        $(this).attr("src","dog.png")
+    })
+</script>
+
+
+<!--7. jquery tausta muutmine-->
+
+
+
 
 
 </body>
