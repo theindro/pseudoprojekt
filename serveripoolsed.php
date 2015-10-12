0753789ea04f00989540b07b40db32c09665c683
Indro <indro.malleus@khk.ee>
2015-10-12 13:13:00 +0300

    1429962-104877158 15. Boolean ylesanne tehtud

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -5,10 +5,14 @@
 $muutuja1 = "Indro";
 $muutuja2 = "Malleus";
 $muutuja3 = 20;
+$eesnimi_algab_vokaaliga = false;
 
 echo $muutuja1." ".$muutuja2, " ($muutuja3)";
 
 // 14. Lisada juurde täisarv tüüpi muutuja, milles on sinu vanus ja kuvada ka see nime taga sulgudes. //
+
+
+// 15. Lisada juurde boolean tüüpi muutuja nimega eesnimi_algab_vokaaliga ja literaalse väärtusega true või false, vastavalt sellele, kas sinu nimi algab vokaaliga või mitte //
 
 
 
