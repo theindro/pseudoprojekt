993854efe94d62a3148874b9edffc03048a350cb
Indro <indro.malleus@khk.ee>
2015-10-12 12:53:50 +0300

    1429962-104877112 13. Stringid

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Indro
- * Date: 05.10.2015
- * Time: 12:53
- */
 
 // 13. Luua PHP fail nimega serveripoolsed.php, mis kuvab ekraanil sinu eesnime ja perekonnanime. Kumbki nimi kuvatakse eraldi PHP muutujatest, mis on string tüüpi. //
 
