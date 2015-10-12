fcaf09f4d5e75202b64c5f05672adde34c03ec18
Indro <indro.malleus@khk.ee>
2015-10-12 14:01:59 +0300

    1429962-104877178 16. Massiivid

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -2,18 +2,17 @@
 
 // 13. Luua PHP fail nimega serveripoolsed.php, mis kuvab ekraanil sinu eesnime ja perekonnanime. Kumbki nimi kuvatakse eraldi PHP muutujatest, mis on string tüüpi. //
 
-$muutuja1 = "Indro";
-$muutuja2 = "Malleus";
-$muutuja3 = 20;
-$eesnimi_algab_vokaaliga = false;
-
-echo $muutuja1." ".$muutuja2, " ($muutuja3)";
 
 // 14. Lisada juurde täisarv tüüpi muutuja, milles on sinu vanus ja kuvada ka see nime taga sulgudes. //
 
 
 // 15. Lisada juurde boolean tüüpi muutuja nimega eesnimi_algab_vokaaliga ja literaalse väärtusega true või false, vastavalt sellele, kas sinu nimi algab vokaaliga või mitte //
 
+// 16. Refaktoorida skript selliselt, et ees-, perenimi ja vanus asuvad $isik nimelises massiivis liikmetena, mille võtmed on vastavalt eesnimi ja perenimi ja vanus. Kuvada sama teksti (ehk nime koos vanusega sulgudes) välja nüüd sellest massiivist.//
 
+
+$isik = array('Indro', 'Malleus', '(20)');
+
+echo "$isik[0] $isik[1] $isik[2] <br>";
 
 ?>
