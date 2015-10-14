3cd13bb97807bfd74c3bed078d5086cbdc74fdb2
Indro <indro.malleus@khk.ee>
2015-10-14 09:35:10 +0300

    1429962-105457214 19. Funktsioonid done

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -47,3 +47,17 @@ echo "<p>" . $inimene->mina["ees"] . " " . $inimene->mina["pere"] . " (" . $inim
 ?>
 
 
+<?php
+
+// 19. nimi algab vokaaliga: //
+
+$vowels = array('A', 'E', 'I', 'O', 'U', 'Õ', 'Ä', 'Ö', 'Ü', 'a', 'e', 'i', 'o', 'u', 'õ', 'ä', 'ö', 'ü');
+if (in_array(substr($inimene->mina["ees"], 0, 1), $vowels)) {
+        $eesnimi_algab_vokaaliga = true;
+    } else {
+    $eesnimi_algab_vokaaliga = false;
+}
+if ($eesnimi_algab_vokaaliga) {
+    echo "<p>Nimi algab vokaaliga</p>";
+}
+?>
